b1eed1328afa8fb5d96938554b76de4a2d4199a5
TheDogHusky <leredlabo@gmail.com>
2022-10-15 12:11:20 +0200

    Optimized lang loading

--- src/structures/LanguageManager.ts
+++ src/structures/LanguageManager.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+import fs from 'fs';
 
 export class LanguageManager {
 	public trads: Map<string, Map<string, string>> = new Map<string, Map<string, string>>();
@@ -6,20 +7,17 @@ export class LanguageManager {
 	public enTrads: Map<string, string> = new Map<string, string>();
 
 	constructor() {
-		const frJson = require('../../languages/fr.json');
-		Object.keys(frJson).forEach(key => {
-			const value = frJson[key];
-			this.frTrads.set(key, value);
-		});
-
-		const enJson = require('../../languages/en.json');
-		Object.keys(enJson).forEach(key => {
-			const value = enJson[key];
-			this.enTrads.set(key, value);
-		});
-
-		this.trads.set('fr', this.frTrads);
-		this.trads.set('en', this.enTrads);
+		const trads = fs.readdirSync("../../languages").filter((file) => file.endsWith(".json"));
+		for (const trad of trads) {
+			const lang = trad.split(".")[0];
+			const tradFile = require(`../../languages/${trad}`);
+			const tradName = lang + "Trads";
+			Object.keys(tradFile).forEach(key => {
+				const value = tradFile[key];
+				this[tradName].set(key, value);
+			});
+			this.trads.set(lang, this[tradName]);
+		}
 	}
 
 	public get(lang: string): Map<string, string> {
